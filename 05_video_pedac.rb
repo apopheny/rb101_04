c42fd3620005c893924dfac4380e772ad10484bb
Chris Douglass <cedouglass@gmail.com>
2022-01-07 21:23:13 -0500

    I SOLVED THE PEDAC PROBLEM BEFORE WATCHING THE VIDEO YAAYYYY

--- 05_video_pedac.rb
+++ 05_video_pedac.rb
@@ -2,7 +2,7 @@
 
 # **Rules:**
 # Sequence begins with two
-# Each row contains the same number of elements as the row number from the top
+# Each row contains the same number of elements as the row depth
 # Each additional element increases by two and this does not reset when a new row begins
 
 # Input: An integer representing the row number from the top
@@ -13,12 +13,23 @@
 # **ALGORITHM**
 
 # Initialize an empty array
-# Receive the input integer representing the row number from the top
-# From 1 until the input integer, output arrays which count by two starting with two, each row the length of the row integer
-  # 1 time +2
-  # 2 times 1.last 
-# Sum up and return the last array
 
+# Receive a number represent the number of rows
+# Initialize a counter variable starting with 1
+# Initialize a variable containting an integer, starting with 0
+# Initialize an outer array
+# Initialize an inner array
+# Begin loop
+#   Add two to the integer variable
+#   Push variable to inner array
+#   Do this counter times
+#   Push inner array to outer array
+#   Integer to equal last element of subarray
+#   Subarray reset to empty subarray
+#   Counter variable increases by one
+# #   End loop when counter is equal to input integer
+# Return outer array
+# Sum the last element in the outer array
 
 def math_solution(int)
   start_val = (int * (int - 1)) + 2
@@ -34,27 +45,31 @@ def math_solution(int)
 end
 
 def all_rows(int)
-  arr = [[0]]
-  start_val = arr.last.last
-  row_counter = 0
-  index_counter = 0
-  until row_counter >= int
-    arr[row_counter][index_counter] += 2    
-    row_counter += 1
-    index_counter += 1
+  arr = []
+  counter = 1
+  last_element = 0
+  temp_arr = [] 
+  loop do
+    break if counter > int
+        counter.times do 
+          last_element += 2
+          temp_arr.push(last_element)
+        end
+    arr.push(temp_arr)
+    last_element = temp_arr.last
+    temp_arr = []
+    counter += 1
   end
- p arr
+  arr
 end
 
-p math_solution(7) == 350
+def sum_row(int)
+  all_rows(int).last.sum
+end
+
+p math_solution(7) == 350 && math_solution(7) == sum_row(7)
 p math_solution(6) == 222 
-all_rows(2)
-
-
-# 01    02      02	      2                         1   +1
-# 02    05      10	      4 6                       3   +2
-# 03    10      30	      8 10 12                   5   +2
-# 04    17      68	      14 16 18 20               7
-# 05    26      130     	22 24 26 28 30            9
-# 06    37      222     	32 34 36 38 40 42         11
-# 07    50      350     	44 46 48 50 52 54 56      13
+p all_rows(1)
+p all_rows(2)
+p all_rows(3)
+p sum_row(7)
